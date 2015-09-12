9c0de6c11e71bd60f783882b45ee2512423e536a
Kim Merino <dafabulousteach@gmail.com>
2015-09-12 16:42:55 -0700

    DOM renders Activity Feed heading

---
+++ client/src/components/activityFeed.js
@@ -0,0 +1,9 @@
+var ActivityFeed = React.createClass({
+  render: function(){
+    return (
+      <h1>Activity Feed</h1>
+    ) 
+  }
+})
+
+React.render(<ActivityFeed/>, document.getElementById('activityfeed'))

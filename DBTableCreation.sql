1a0859f2fc55c03e0c9b3ee81ab94c419c1c6e8a
jdlawrence <jamil.lawrence@gmail.com>
2015-09-12 16:24:30 -0700

    Added users_habits junction table and file for creation of appropriate schema

---
+++ DBTableCreation.sql
@@ -0,0 +1,33 @@
+DROP TABLE updates;
+DROP TABLE users_habits;
+DROP TABLE habits;
+DROP TABLE users;
+
+CREATE TABLE users
+(
+  user_id serial NOT NULL PRIMARY KEY,
+  username character varying(200),
+  password character varying(50),
+  location character varying(200) DEFAULT 'Berkeley'
+);
+
+CREATE TABLE habits
+(
+  habit_id serial NOT NULL PRIMARY KEY,
+  habit character varying(200),
+  category character varying(200)
+);
+
+CREATE TABLE users_habits
+(
+  user_habit_id serial NOT NULL PRIMARY KEY,
+  user_id integer NOT NULL REFERENCES users (user_id),
+  habit_id integer NOT NULL REFERENCES habits (habit_id)
+);
+
+CREATE TABLE updates
+(
+  update_id serial NOT NULL PRIMARY KEY,
+  habit_id INTEGER NOT NULL REFERENCES habits (habit_id),
+  update_time TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
+);

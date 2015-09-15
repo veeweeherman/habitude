dd22e51076d9addc839073a2d3a2058c23d09886
veeweeherman <vy.hoang.cu@gmail.com>
2015-09-14 19:05:21 -0700

    nested the components on activity feed

--- client/src/components/activityFeed.js
+++ client/src/components/activityFeed.js
@@ -1,7 +1,5 @@
-
-
-var ActivityFeed = React.createClass({
-	giveKudos: function(){ // method gets fires on button-click
+var Activities = React.createClass({
+		giveKudos: function(){ // method gets fires on button-click
 		$.ajax({
       url: '/api/giveKudos',
       type: 'PUT',   
@@ -19,12 +17,49 @@ var ActivityFeed = React.createClass({
 
 	render: function(){
     return (
-    	<div>
-	      <h3>Activity Feed</h3>
-	      <button type="submit" onClick={this.giveKudos.bind(this)}>THUMBS UP</button>
+    	<div >
+	  		<button type="submit" onClick={this.giveKudos.bind(this)}>THUMBS UP</button>
       </div>
     ) 
   }
 })
+// <p>{username} checked in today for: {biking}. {username} did it on: {2015-09-14}</p>
+var ActivityFeed = React.createClass({ //parent component
+	loadActivitiesFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        // console.error(this.props.url);
+        // console.error(status);
+        // console.log(err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadActivitiesFromServer();
+    setInterval(this.loadActivitiesFromServer, this.props.pollInterval); // retrieves habits from db on interval
+  },
+  render: function() {
+    return (
+      <div className="ActivityFeed">
+      <h1>Activity Feed</h1>
+      	<table><tbody>
+	  			<tr>
+	      	<td>{this.props.username} checked in today for: {this.props.habit}. {this.props.username} did it on: {this.props.date}</td>
+	      	<td><Activities /></td>
+	      	</tr>
+	  		</tbody></table>
+      </div>
+      );
+  }
+})
 
-React.render(<ActivityFeed/>, document.getElementById('activityfeed'))
+React.render(<ActivityFeed username={'glennrique'} habit={'droppin beats'} date={'2015-09-14'} />, document.getElementById('activityfeed'))

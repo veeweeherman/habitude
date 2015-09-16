9a5b634539e2e423655ca2720eb7a582c65adfc4
veeweeherman <vy.hoang.cu@gmail.com>
2015-09-16 12:20:27 -0700

    added documentation

--- client/src/components/activityFeed.js
+++ client/src/components/activityFeed.js
@@ -1,31 +1,3 @@
-// var KudosButton = React.createClass({
-// 		giveKudos: function(updateID){ // method gets fires on button-click
-//       console.log('updateID',updateID)
-// 		$.ajax({
-//       url: '/api/giveKudos',
-//       type: 'POST',   // PUT request to update kudos_count value in the db
-//       contentType: 'application/json',
-//       dataType: 'json',
-//       success: function(data) { // IDK why no success msg sent but in console success message from db-query
-//       	this.setState({data:data})
-//         console.log('successful thumbs up')
-//       }.bind(this),
-//       error: function(xhr, status, err) {
-//         console.error(this.props.url, status, err.toString());
-//       }.bind(this)
-//     });
-// 	},
-
-// 	render: function(){
-//     console.log('what is this',this)
-//     return (
-//     	<div >
-// 	  		<button type="submit" onClick={this.giveKudos.bind(activity)}>THUMBS UP</button>
-//       </div>
-//     ) 
-//   }
-// })
-
 var Counter = React.createClass({ // child component, see https://scotch.io/tutorials/learning-react-getting-started-and-concepts for more info
   incrementCount: function(){
     this.setState({
@@ -49,7 +21,7 @@ var Counter = React.createClass({ // child component, see https://scotch.io/tuto
 
 var ActivityFeed = React.createClass({ //parent component
 
-	loadActivitiesFromServer: function() { //loads data for the activity feed
+  loadActivitiesFromServer: function() { //loads data for the activity feed
     $.ajax({
       url: '/api/activityFeed',
       dataType: 'json',
@@ -89,7 +61,7 @@ var ActivityFeed = React.createClass({ //parent component
     var activityNodes = this.state.data.map(function(activity,i,list){ //iterating thru array of objects returned from db query, and gives each activity its own kudos button
       return (
       
-        	<table><tbody>
+          <table><tbody>
             <tr>
               <td>{activity.username+': '+activity.habit+' at '+activity.update_time}</td>
               <td><Counter/></td>
@@ -104,3 +76,36 @@ var ActivityFeed = React.createClass({ //parent component
 })
 
 React.render(<ActivityFeed />, document.getElementById('activityfeed'))
+
+/*************************************************
+IF KUDOS BUTTON IS BETTER OFF AS ITS OWN COMPONENT, 
+SAVED IT HERE IN CASE IT NEEDS TO BE REFACTORED IN
+:)
+*************************************************/
+// var KudosButton = React.createClass({
+// 		giveKudos: function(updateID){ // method gets fires on button-click
+//       console.log('updateID',updateID)
+// 		$.ajax({
+//       url: '/api/giveKudos',
+//       type: 'POST',   // PUT request to update kudos_count value in the db
+//       contentType: 'application/json',
+//       dataType: 'json',
+//       success: function(data) { // IDK why no success msg sent but in console success message from db-query
+//       	this.setState({data:data})
+//         console.log('successful thumbs up')
+//       }.bind(this),
+//       error: function(xhr, status, err) {
+//         console.error(this.props.url, status, err.toString());
+//       }.bind(this)
+//     });
+// 	},
+
+// 	render: function(){
+//     console.log('what is this',this)
+//     return (
+//     	<div >
+// 	  		<button type="submit" onClick={this.giveKudos.bind(activity)}>THUMBS UP</button>
+//       </div>
+//     ) 
+//   }
+// })

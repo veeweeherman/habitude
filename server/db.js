3aa3721ae9a412d839f37ac8f588af2ef70451a2
Kim Merino <dafabulousteach@gmail.com>
2015-09-18 16:57:57 -0700

    removes commented code not needed

--- server/db.js
+++ server/db.js
@@ -40,7 +40,6 @@ module.exports = function(app){
   // and the updates table
   app.post('/api/habits', function(req, res){
     var habit = req.body.habit;
-    //*********************** NOTE: 'category' is currently hardcoded to be 'health'
     var category = req.body.category;
 
 
@@ -52,23 +51,18 @@ module.exports = function(app){
     var user = 'Vy'; 
 
     pg.connect(databaseURL, function(err, client, done){
-
-      // Currently we only post habits for user number 1: Later we will add multiple users
-      // var query = client.query("INSERT INTO habits (user_id, habit) VALUES ($1, $2)", [1, habit]);
       var habitIDQuery = "(SELECT DISTINCT habits.habit_id FROM habits " + 
        "WHERE habits.habit = '" + habit + "')"; 
 
       var userIDQuery = "(SELECT DISTINCT users.user_id FROM users " + 
        "WHERE users.username = '" + user + "')"; 
 
-    var habitsQuery = client.query("INSERT INTO habits(habit, category) VALUES ( '" + habit + "', '" + category + "'); " +
+      var habitsQuery = client.query("INSERT INTO habits(habit, category) VALUES ( '" + habit + "', '" + category + "'); " +
                                    "INSERT INTO users_habits (user_id, habit_id) VALUES (" + userIDQuery + ", " + habitIDQuery + "); " +
                                    "INSERT INTO updates (habit_id, update_time) " + 
                                    "VALUES (" + habitIDQuery + " , current_timestamp);");
 
-    done();
-      // Array to hold values returned from database
-      
+      done();
       var rows = []; 
       if (err) {
         return console.error('error running query', err);
@@ -85,9 +79,6 @@ module.exports = function(app){
 
   // GET USER UPDATES TIMES AND FREQUENCY 
   app.get('/api/updateHabit', function(req, res){
-    // Returns a JSON object with all habits and a count of how many times they occur
-    // Example: [{"habit":"trapping","count":"2"},{"habit":"biking","count":"9"}]
-    // CURL COMMAND: curl -i localhost:3000/api/updateHabit
     pg.connect(databaseURL, function(err, client, done){
       var query = client.query("SELECT habits.habit, count(updates.habit_id) " + 
        "FROM habits " + 
@@ -117,10 +108,6 @@ module.exports = function(app){
   app.post('/api/updateHabit', function(req, res){
     var habit = req.body.habit;
     pg.connect(databaseURL, function(err, client, done){
-      // Posts an update to the 'updates' table where the habit_id matches that of the input habit string
-      // CURL COMMAND: curl -X POST -d "habit='biking'" localhost:3000/api/updateHabit
-      // will update the 'biking' habit
-
       var getIDQuery = "(SELECT DISTINCT habits.habit_id FROM habits " + 
        "WHERE habits.habit = '" + habit + "')";
     var query = client.query("INSERT INTO updates (habit_id) " +
@@ -168,7 +155,7 @@ module.exports = function(app){
     });
 
 
-//VY AND GLENNs DB request for name and location
+// DB request for name and location
   app.get('/api/nameAndLoc', function(req, res) {
     pg.connect(databaseURL, function (err, client, done){      
       var query = client.query("SELECT location,username FROM users WHERE user_id = 1;");
@@ -190,7 +177,9 @@ module.exports = function(app){
 
   // SHOWS ACTIVITY FEED FROM OTHER USERS IN THE SAME CATEGORY
   app.get('/api/activityFeed', function (req, res){
+
     var user = 'Vy';
+
     pg.connect(databaseURL, function (err, client, done){
 
       var getCategories = "(SELECT DISTINCT habits.category "+
@@ -231,12 +220,10 @@ module.exports = function(app){
       });
     });
   });
-// VY and GLENN request for give kudos/increment the update's kudos_count
-// hardcoded for update with update_id = 1
+
   app.post('/api/giveKudos', function(req, res){
     var update_id = req.body.update_id;
     pg.connect(databaseURL, function(err, client, done){
-      // var query = client.query("UPDATE updates SET kudos_count = kudos_count + 1 WHERE update_id = " + update_id + ";");
       var query = client.query("UPDATE updates SET kudos_count = kudos_count + 1 WHERE update_id = 1;");
       done();
       var rows = [];

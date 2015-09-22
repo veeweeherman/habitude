8276ad33015db351ac03fd7b982f2239f149e118
Glenn Gonda <glenngonda@gmail.com>
2015-09-22 12:26:56 -0700

    changing config back to db url

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     /* Change the database name to your local machine's name*/
 
-    databaseURL: process.env.DATABASE_URL || 'postgres://localhost:5432/glenngonda'
+    // databaseURL: process.env.DATABASE_URL || 'postgres://localhost:5432/glenngonda'
 
 
     // databaseURL: process.env.DATABASE_URL || 'postgres://localhost:5432/kmerino'
@@ -19,5 +19,5 @@ module.exports = {
     If the Heroku toolbelt and the PGAdmin are both installed, run the following to access the PG DB in the terminal: 
       heroku pg:psql --app habitudein30 HEROKU_POSTGRESQL_TEAL
     */
-    // databaseURL: process.env.DATABASE_URL || 'postgres://qacakftnxcikfj:fppgETRZp0te0xzfcKgihjywYN@ec2-54-243-149-147.compute-1.amazonaws.com:5432/d4etla2a7iv9n3'
+    databaseURL: process.env.DATABASE_URL || 'postgres://qacakftnxcikfj:fppgETRZp0te0xzfcKgihjywYN@ec2-54-243-149-147.compute-1.amazonaws.com:5432/d4etla2a7iv9n3'
 };
